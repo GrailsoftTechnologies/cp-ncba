b382e6c6c390001a5c3408910440d69aa74feea8
David Cuthell <dcuthell@pdx.edu>
2018-07-10 12:17:49 -0700

    add agenda and full cal modes for calendar component

--- src/components/CalendarFrame/index.js
+++ src/components/CalendarFrame/index.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
+import "./styles.css" ;
 
 class CalendarFrame extends Component {
-  render() {
 
-    return (
-      <div>
-        <iframe id="calendar" src="https://calendar.google.com/calendar/embed?src=i_131.252.200.188%23sunrise%40group.v.calendar.google.com&ctz=America%2FLos_Angeles" width="800" height="600" frameborder="0" scrolling="no"></iframe>
-      </div>
-    );
+
+  render() {
+		if (this.props.list) {
+			return (
+				<div>
+					<iframe src="https://calendar.google.com/calendar/embed?showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23e3e9ff&amp;src=i_131.252.200.188%23sunrise%40group.v.calendar.google.com&amp;color=%235A6986&amp;ctz=America%2FChicago"  width="400" height="600" frameborder="0" scrolling="no"></iframe>
+				</div>
+			);
+		} else {
+			return (
+				<div>
+					<iframe src="https://calendar.google.com/calendar/embed?height=800&amp;wkst=1&amp;bgcolor=%23e3e9ff&amp;src=i_131.252.200.188%23sunrise%40group.v.calendar.google.com&amp;color=%235A6986&amp;ctz=America%2FChicago"  width="800" height="800" frameborder="0" scrolling="no"></iframe>
+				</div>
+			)
+		}
+
   }
+}
+
+CalendarFrame.defaultProps = {
+	width : 800,
+	height : 1600,
+	list: true,
+}
+
+CalendarFrame.propTypes = {
+
 }
 
 export default CalendarFrame;
