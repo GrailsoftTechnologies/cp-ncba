6c0f0c3c85caccfdfb063da9039c810895ac57fd
David Cuthell <dcuthell@pdx.edu>
2018-07-11 16:22:36 -0700

    add router to index.js, add libraries for router and bootstrap

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'react-bootstrap';
 import './index.css';
 import App from './App';
+import { HashRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+  <HashRouter basename={ process.env.PUBLIC_URL }>
+    <App />
+  </HashRouter>, document.getElementById('root'));
 registerServiceWorker();
