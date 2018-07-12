60cb36c38a83014129b77dde17ed1bf5fd99255e
David Cuthell <dcuthell@pdx.edu>
2018-07-11 17:44:26 -0700

    setup basic navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,27 +1,34 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, Nav, NavItem, NavbarToggler, Collapse, NavbarBrand } from 'reactstrap';
+import { Navbar, Nav, NavItem, NavbarToggler, Collapse, NavbarBrand, NavLink } from 'reactstrap';
 import './styles.css';
 import Logo from '../../img/image2.jpeg';
 
 class NavBar extends Component {
+	constructor(props){
+		super(props);
+		this.toggle = this.toggle.bind(this);
+		this.state = {
+			isOpen : false
+		}
+	}
+	toggle(){
+		this.setState({
+			isOpen : !this.state.isOpen
+		})
+	}
   render() {
     return (
-      <Navbar inverse fluid collapseOnSelect className={this.props.className}>
-          <NavbarBrand>
-            <Link to="/"><img id="bamLogo" src={Logo} alt='the brick and motor logo'></img></Link>
-          </NavbarBrand>
-          <NavbarToggler />
-        <Collapse>
-          <Nav pullLeft>
-            <NavItem href={"#/Menu"} className="menuItem">Menu</NavItem>
-            <NavItem href={"#/About"} className="menuItem">About</NavItem>
-            <NavItem href={"#/Contacts"} className="menuItem">Contact</NavItem>
-          </Nav>
-          <Nav pullRight>
-            <NavItem href="https://www.instagram.com/brickandmotorpdx/" target="_blank" rel="noopener noreferrer"><span className="footer-icon icon fa fa-instagram desktop-only"></span></NavItem>
-            <NavItem href="https://www.facebook.com/brickandmotorpdx/" target="_blank" rel="noopener noreferrer"><span className="footer-icon icon fa fa-facebook-square desktop-only"></span></NavItem>
-            <NavItem href="https://www.twitter.com" target="_blank" rel="noreferrer noopener"><span className="footer-icon icon fa fa-twitter-square desktop-only"></span></NavItem>
+      <Navbar inverse fluid collapseOnSelect expand="md" className={this.props.className}>
+        <NavbarBrand>
+          <Link to="/"><img id="bamLogo" src={Logo} alt='the brick and motor logo'></img></Link>
+        </NavbarBrand>
+        <NavbarToggler onClick={this.toggle}/>
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavLink href={"#/Menu"} className="menuItem">Menusss</NavLink>
+            <NavLink href={"#/About"} className="menuItem">About</NavLink>
+            <NavLink href={"#/Contacts"} className="menuItem">Contact</NavLink>
           </Nav>
         </Collapse>
       </Navbar>
