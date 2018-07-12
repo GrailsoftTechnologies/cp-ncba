9e078574a57a043d8154d5858eabcd3a47f57c63
David Cuthell <dcuthell@pdx.edu>
2018-07-11 17:25:48 -0700

    add some navbar items in reactstrap

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { Navbar, Nav, NavItem, NavbarToggler, Collapse, NavbarBrand } from 'reactstrap';
 import './styles.css';
 import Logo from '../../img/image2.jpeg';
 
@@ -8,13 +8,11 @@ class NavBar extends Component {
   render() {
     return (
       <Navbar inverse fluid collapseOnSelect className={this.props.className}>
-        <Navbar.Header>
-          <Navbar.Brand>
+          <NavbarBrand>
             <Link to="/"><img id="bamLogo" src={Logo} alt='the brick and motor logo'></img></Link>
-          </Navbar.Brand>
-          <Navbar.Toggle/>
-        </Navbar.Header>
-        <Navbar.Collapse>
+          </NavbarBrand>
+          <NavbarToggler />
+        <Collapse>
           <Nav pullLeft>
             <NavItem href={"#/Menu"} className="menuItem">Menu</NavItem>
             <NavItem href={"#/About"} className="menuItem">About</NavItem>
@@ -25,7 +23,7 @@ class NavBar extends Component {
             <NavItem href="https://www.facebook.com/brickandmotorpdx/" target="_blank" rel="noopener noreferrer"><span className="footer-icon icon fa fa-facebook-square desktop-only"></span></NavItem>
             <NavItem href="https://www.twitter.com" target="_blank" rel="noreferrer noopener"><span className="footer-icon icon fa fa-twitter-square desktop-only"></span></NavItem>
           </Nav>
-        </Navbar.Collapse>
+        </Collapse>
       </Navbar>
     );
   }
