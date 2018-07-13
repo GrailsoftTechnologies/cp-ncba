3d07cd354d50d5a4fbde2164e335f554e7dfef08
David Cuthell <dcc1447@gmail.com>
2018-07-13 15:34:01 -0700

    add jumbotrons to grid

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './styles.css';
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col, Jumbotron, Button } from 'reactstrap';
 import CarouselFrame from '../CarouselFrame';
 
 
@@ -12,26 +12,74 @@ class Home extends Component {
         <Container>
           <Row>
             <Col>
-              <p>1</p>
+              <Jumbotron>
+                <h1 className="display-3">Hello, world!</h1>
+                <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+                <hr className="my-2" />
+                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                <p className="lead">
+                  <Button color="primary">Learn More</Button>
+                </p>
+              </Jumbotron>
             </Col>
           </Row>
           <Row>
             <Col>
-              <p>2</p>
+              <Jumbotron>
+                <h1 className="display-3">Hello, world!</h1>
+                <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+                <hr className="my-2" />
+                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                <p className="lead">
+                  <Button color="primary">Learn More</Button>
+                </p>
+              </Jumbotron>
             </Col>
             <Col>
-              <p>3</p>
+              <Jumbotron>
+                <h1 className="display-3">Hello, world!</h1>
+                <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+                <hr className="my-2" />
+                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                <p className="lead">
+                  <Button color="primary">Learn More</Button>
+                </p>
+              </Jumbotron>
             </Col>
           </Row>
           <Row>
             <Col>
-              <p>4</p>
+              <Jumbotron>
+                <h1 className="display-3">Hello, world!</h1>
+                <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+                <hr className="my-2" />
+                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                <p className="lead">
+                  <Button color="primary">Learn More</Button>
+                </p>
+              </Jumbotron>
             </Col>
             <Col>
-              <p>5</p>
+              <Jumbotron>
+                <h1 className="display-3">Hello, world!</h1>
+                <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+                <hr className="my-2" />
+                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                <p className="lead">
+                  <Button color="primary">Learn More</Button>
+                </p>
+              </Jumbotron>
             </Col>
             <Col>
-              <p>6</p>
+              <Jumbotron>
+                <h1 className="display-3">Hello, world!</h1>
+                <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+                <hr className="my-2" />
+                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                <p className="lead">
+                  <Button color="primary">Learn More</Button>
+                </p>
+              </Jumbotron>
             </Col>
           </Row>
         </Container>
