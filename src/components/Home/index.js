97ed6030d997cf1e78306daa1423b6de014b51f2
David Cuthell <dcc1447@gmail.com>
2018-07-13 15:29:27 -0700

    add grid to home component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './styles.css';
-
+import { Container, Row, Col } from 'reactstrap';
 import CarouselFrame from '../CarouselFrame';
 
 
@@ -9,6 +9,32 @@ class Home extends Component {
 		return(
 			<div className="Home">
 				<CarouselFrame/>
+        <Container>
+          <Row>
+            <Col>
+              <p>1</p>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <p>2</p>
+            </Col>
+            <Col>
+              <p>3</p>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <p>4</p>
+            </Col>
+            <Col>
+              <p>5</p>
+            </Col>
+            <Col>
+              <p>6</p>
+            </Col>
+          </Row>
+        </Container>
 			</div>
 		)
   }
