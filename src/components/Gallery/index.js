26b9fd341962ee79bfdce9d6554300f0d77ff43c
David Cuthell <dcc1447@gmail.com>
2018-07-13 16:07:52 -0700

    add media objects to gallery, sort of

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './styles.css';
+import { Media } from 'reactstrap';
 
 import YouTubeFrame from '../YouTubeFrame';
 
@@ -8,7 +9,29 @@ render() {
   return(
   	<div>
 			<p>Gallery</p>
-			<YouTubeFrame/>
+
+      <Media>
+       <Media right href="#">
+        <YouTubeFrame/>
+       </Media>
+       <Media body>
+         <Media heading>
+           Media heading
+         </Media>
+         Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+       </Media>
+     </Media>
+     <Media>
+      <Media right href="#">
+       <YouTubeFrame/>
+      </Media>
+      <Media body>
+        <Media heading>
+          Media heading
+        </Media>
+        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+      </Media>
+    </Media>
 		</div>
     );
   }
