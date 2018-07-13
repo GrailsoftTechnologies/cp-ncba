fa27653f134e5e9e37993744b7541ad5cf780f01
David Cuthell <dcc1447@gmail.com>
2018-07-13 15:55:13 -0700

    add cards to grid

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './styles.css';
-import { Container, Row, Col, Jumbotron, Button } from 'reactstrap';
+import { Container, Row, Col, Jumbotron, Button,
+  Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
 import CarouselFrame from '../CarouselFrame';
 
 
@@ -49,37 +50,37 @@ class Home extends Component {
           </Row>
           <Row>
             <Col>
-              <Jumbotron>
-                <h1 className="display-3">Hello, world!</h1>
-                <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
-                <hr className="my-2" />
-                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                <p className="lead">
-                  <Button color="primary">Learn More</Button>
-                </p>
-              </Jumbotron>
+              <Card>
+                <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                <CardBody>
+                  <CardTitle>Card title</CardTitle>
+                  <CardSubtitle>Card subtitle</CardSubtitle>
+                  <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                  <Button>Button</Button>
+                </CardBody>
+              </Card>
             </Col>
             <Col>
-              <Jumbotron>
-                <h1 className="display-3">Hello, world!</h1>
-                <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
-                <hr className="my-2" />
-                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                <p className="lead">
-                  <Button color="primary">Learn More</Button>
-                </p>
-              </Jumbotron>
+              <Card>
+                <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                <CardBody>
+                  <CardTitle>Card title</CardTitle>
+                  <CardSubtitle>Card subtitle</CardSubtitle>
+                  <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                  <Button>Button</Button>
+                </CardBody>
+              </Card>
             </Col>
             <Col>
-              <Jumbotron>
-                <h1 className="display-3">Hello, world!</h1>
-                <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
-                <hr className="my-2" />
-                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                <p className="lead">
-                  <Button color="primary">Learn More</Button>
-                </p>
-              </Jumbotron>
+              <Card>
+                <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                <CardBody>
+                  <CardTitle>Card title</CardTitle>
+                  <CardSubtitle>Card subtitle</CardSubtitle>
+                  <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                  <Button>Button</Button>
+                </CardBody>
+              </Card>
             </Col>
           </Row>
         </Container>
