8fd79263eefd7c81823786c5d4bebb00debf851f
David Cuthell <dcc1447@gmail.com>
2018-07-17 13:27:20 -0700

    change styling for media icons in footer, added links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -14,7 +14,7 @@ render() {
 					<Col xs="2">
 						<img id="footerLogo" src={Logo} alt="footer logo" />
 					</Col>
-					<Col xs="4" className="addressBox">
+					<Col xs="7" className="addressBox">
 						<p><strong>Nashville Classical Ballet Academy</strong></p>
 						<p>123 Main St.</p>
 						<p>Nashville TN 12345</p>
@@ -22,12 +22,9 @@ render() {
 					</Col>
 					<Col xs="3">
 						<a href="http://www.facebook.com"><i className="mediaIcon fab fa-facebook"></i></a>
-						<i className="mediaIcon fab fa-instagram"></i>
-						<i className="mediaIcon fab fa-twitter"></i>
-						<i className="mediaIcon fab fa-youtube"></i>
-					</Col>
-					<Col xs="3">
-						<p>Three Four</p>
+						<a href="http://www.instagram.com"><i className="mediaIcon fab fa-instagram"></i></a>
+						<a href="http://www.twitter.com"><i className="mediaIcon fab fa-twitter"></i></a>
+						<a href="http://www.youtube.com"><i className="mediaIcon fab fa-youtube"></i></a>
 					</Col>
 				</Row>
 			</Container>
