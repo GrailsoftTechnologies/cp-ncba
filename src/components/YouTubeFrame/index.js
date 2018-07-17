165d044b4afd47cd781bb45a0366ce0e4ecc9b24
David Cuthell <dcuthell@pdx.edu>
2018-07-16 21:12:06 -0700

    add border to youtubeframe

--- src/components/YouTubeFrame/index.js
+++ src/components/YouTubeFrame/index.js
@@ -7,7 +7,7 @@ class YouTubeFrame extends Component {
       <div>
         <iframe id="player" type="text/html" width="640" height="390"
   src="http://www.youtube.com/embed?listType=playlist&list=PLXeXlqUgjSWNAZFPJB6rf7uOBUtvEjfJD"
-  frameborder="0" title="YTplaylist1"></iframe>
+  frameBorder="0" title="YTplaylist1"></iframe>
       </div>
     );
   }
