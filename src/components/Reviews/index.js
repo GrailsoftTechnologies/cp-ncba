f383e4d0e3ef4fa0bd2239e7deeb53063988cf78
David Cuthell <dcuthell@pdx.edu>
2018-07-16 20:56:31 -0700

    add contenttostate method for review page

--- src/components/Reviews/index.js
+++ src/components/Reviews/index.js
@@ -2,28 +2,32 @@ import React, { Component } from 'react';
 import ReactHtmlParser from 'react-html-parser';
 import './styles.css';
 
-const testString = "This is a post. I'm posting things. Also adding a picture. Its pretty cool.<br />\n<span style=\"background-color: white; font-family: &quot;open sans&quot; , &quot;arial&quot; , sans-serif; font-size: 14px; text-align: justify;\"><br /></span>\n<span style=\"background-color: white; font-family: &quot;open sans&quot; , &quot;arial&quot; , sans-serif; font-size: 14px; text-align: justify;\"><br /></span>\n<br />\n<div class=\"separator\" style=\"clear: both; text-align: center;\">\n<a href=\"https://2.bp.blogspot.com/-7s6yNWaPWCE/W0kU0JAGpnI/AAAAAAAAAiE/ogG68YFkfu0IkQ_kM5_PoeM6BH2mdI5ggCLcBGAs/s1600/2018-07-03%2B11.28.36.jpg\" imageanchor=\"1\" style=\"margin-left: 1em; margin-right: 1em;\"><img border=\"0\" data-original-height=\"1070\" data-original-width=\"1600\" height=\"213\" src=\"https://2.bp.blogspot.com/-7s6yNWaPWCE/W0kU0JAGpnI/AAAAAAAAAiE/ogG68YFkfu0IkQ_kM5_PoeM6BH2mdI5ggCLcBGAs/s320/2018-07-03%2B11.28.36.jpg\" width=\"320\" /></a></div>\n<span style=\"background-color: white; font-family: &quot;open sans&quot; , &quot;arial&quot; , sans-serif; font-size: 14px; text-align: justify;\"><br /></span>";
-
 class Reviews extends Component {
 	constructor(props){
 		super(props);
 
 		this.state = {
-			content : "empty",
+			content : ["empty"],
 		}
 
 	}
 
-	componentDidMount(){
-		fetch("https://www.googleapis.com/blogger/v3/blogs/6105946992458304803/posts/6645798792441277989?key=AIzaSyAxXlgJkFdgB4tiD5xCG9HOjX2ENWgZZlU")
+	mapContentToState(){
+		fetch("https://www.googleapis.com/blogger/v3/blogs/6105946992458304803/posts/?key=AIzaSyAxXlgJkFdgB4tiD5xCG9HOjX2ENWgZZlU")
       .then(res => res.json())
       .then(
         (result) => {
-					console.log(result.content);
-					let newText = result.content;
-          this.setState({
-            content: newText
-          });
+					console.log(result.items);
+					let myMap = result.items;
+					myMap.forEach( (item) => {
+						console.log(item.content);
+						this.setState({
+							content: [
+								...this.state.content,
+								item.content
+							]
+						});
+					});
 					console.log(this.state);
         },
         (error) => {
@@ -33,6 +37,10 @@ class Reviews extends Component {
         }
       )
 			console.log(this.state);
+	}
+
+	componentDidMount(){
+		this.mapContentToState();
 	}
 
 	render() {
