fea9fa054d97dee46ba4c7765fd27f5491d9cc4b
David Cuthell <dcc1447@gmail.com>
2018-07-17 17:23:15 -0700

    make calendar smarter, changed some styling

--- src/components/CalendarFrame/index.js
+++ src/components/CalendarFrame/index.js
@@ -8,13 +8,13 @@ class CalendarFrame extends Component {
 		if (this.props.list) {
 			return (
 				<div>
-					<iframe src="https://calendar.google.com/calendar/embed?showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23e3e9ff&amp;src=i_131.252.200.188%23sunrise%40group.v.calendar.google.com&amp;color=%235A6986&amp;ctz=America%2FChicago"  width="400" height="600" frameborder="0" scrolling="no" title="icalframe1"></iframe>
+					<iframe src="https://calendar.google.com/calendar/embed?showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23e3e9ff&amp;src=i_131.252.200.188%23sunrise%40group.v.calendar.google.com&amp;color=%235A6986&amp;ctz=America%2FChicago"  width={this.props.width} height={this.props.height} frameBorder="0" scrolling="no" title="icalframe1"></iframe>
 				</div>
 			);
 		} else {
 			return (
 				<div>
-					<iframe src="https://calendar.google.com/calendar/embed?height=800&amp;wkst=1&amp;bgcolor=%23e3e9ff&amp;src=i_131.252.200.188%23sunrise%40group.v.calendar.google.com&amp;color=%235A6986&amp;ctz=America%2FChicago"  width="1400" height="800" frameborder="0" scrolling="no" title="icalframe2"></iframe>
+					<iframe src="https://calendar.google.com/calendar/embed?height=800&amp;wkst=1&amp;bgcolor=%23e3e9ff&amp;src=i_131.252.200.188%23sunrise%40group.v.calendar.google.com&amp;color=%235A6986&amp;ctz=America%2FChicago"  width={this.props.width} height={this.props.height} frameBorder="0" scrolling="no" title="icalframe2"></iframe>
 				</div>
 			)
 		}
@@ -23,8 +23,8 @@ class CalendarFrame extends Component {
 }
 
 CalendarFrame.defaultProps = {
-	width : 800,
-	height : 1600,
+	width : 1200,
+	height : 800,
 	list: false,
 }
 
--- src/components/Schedule/index.js
+++ src/components/Schedule/index.js
@@ -5,9 +5,49 @@ import CalendarFrame from '../CalendarFrame';
 
 class Schedule extends Component {
 render() {
+	console.log(window.innerWidth);
+	console.log(window.innerHeight);
+	let winX = 1200;
+	let winY = 1000;
+	if(window.innerWidth < 1200){
+		if(window.innerWidth < 992){
+			if(window.innerWidth < 768){
+				if(window.innerWidth < 576){
+					winX = 300;
+				}
+				else{
+					winX = 550;
+				}
+			}
+			else{
+				winX = 750;
+			}
+		}else{
+			winX = 950;
+		}
+	}
+
+	if(window.innerHeight < 1000){
+		if(window.innerHeight < 792){
+			if(window.innerHeight < 568){
+				if(window.innerHeight < 276){
+					winY = 300;
+				}
+				else{
+					winY = 400;
+				}
+			}
+			else{
+				winY = 600;
+			}
+		}else{
+			winY = 800;
+		}
+	}
+	console.log(winY);
   return(
   	<div>
-			<CalendarFrame/>
+			<CalendarFrame width={winX} height={winY} list={(winX < 568)?true:false}/>
 		</div>
     );
   }
