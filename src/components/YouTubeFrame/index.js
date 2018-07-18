10822310b609023a4e0223ceab387295fc8cc576
David Cuthell <dcc1447@gmail.com>
2018-07-17 19:15:02 -0700

    add height and width props for youtubeframe

--- src/components/YouTubeFrame/index.js
+++ src/components/YouTubeFrame/index.js
@@ -5,12 +5,18 @@ class YouTubeFrame extends Component {
 
     return (
       <div>
-        <iframe id="player" type="text/html" width="640" height="390"
+        <iframe id="player" type="text/html" width={this.props.width} height={this.props.height}
   src="http://www.youtube.com/embed?listType=playlist&list=PLXeXlqUgjSWNAZFPJB6rf7uOBUtvEjfJD"
   frameBorder="0" title="YTplaylist1"></iframe>
       </div>
     );
   }
+}
+
+YouTubeFrame.defaultProps = {
+	src: "http://www.youtube.com/embed?listType=playlist&list=PLXeXlqUgjSWNAZFPJB6rf7uOBUtvEjfJD",
+	width: 640,
+	height: 190,
 }
 
 export default YouTubeFrame;
