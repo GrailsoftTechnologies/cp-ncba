6de8847da53dc5e89ca7945fc003f7257de0cf89
David Cuthell <dcc1447@gmail.com>
2018-07-17 19:10:10 -0700

    changed content of contact component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,84 +1,59 @@
 import React, { Component } from "react";
-import { Button, Form, FormGroup, Label, Input, FormText, Container } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, FormText, Container, Jumbotron } from 'reactstrap';
 import "./styles.css";
 
 class Contact extends Component {
   render() {
     return (
-      <div>
+      <Jumbotron>
         <Container>
           <Form>
-            <FormGroup>
-              <Label for="exampleEmail">Email</Label>
-              <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+						<FormGroup>
+              <Label for="firstName">First Name (Required)</Label>
+              <Input type="text" name="firstName" id="firstName" placeholder="" required/>
+							<Label for="lastName">Last Name</Label>
+              <Input type="text" name="lastName" id="lastName" placeholder="" />
             </FormGroup>
             <FormGroup>
-              <Label for="examplePassword">Password</Label>
-              <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+              <Label for="email">Email (Required)</Label>
+              <Input type="email" name="email" id="email" placeholder="" required/>
             </FormGroup>
             <FormGroup>
-              <Label for="exampleSelect">Select</Label>
-              <Input type="select" name="select" id="exampleSelect">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-              </Input>
-            </FormGroup>
-            <FormGroup>
-              <Label for="exampleSelectMulti">Select Multiple</Label>
-              <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-              </Input>
-            </FormGroup>
-            <FormGroup>
-              <Label for="exampleText">Text Area</Label>
-              <Input type="textarea" name="text" id="exampleText" />
+              <Label for="text">Your Message</Label>
+              <Input type="textarea" name="text" id="text" placeholder="Please enter a title if submitting a document"required/>
             </FormGroup>
             <FormGroup>
               <Label for="exampleFile">File</Label>
               <Input type="file" name="file" id="exampleFile" />
               <FormText color="muted">
-                This is some placeholder block-level help text for the above input.
-                It's a bit lighter and easily wraps to a new line.
+                Files can be submitted and emailed
               </FormText>
             </FormGroup>
             <FormGroup tag="fieldset">
-              <legend>Radio Buttons</legend>
+              <legend>I'm interested in...</legend>
               <FormGroup check>
                 <Label check>
                   <Input type="radio" name="radio1" />{' '}
-                  Option one is this and that—be sure to include why it's great
+                  Learning more about the school
                 </Label>
               </FormGroup>
               <FormGroup check>
                 <Label check>
                   <Input type="radio" name="radio1" />{' '}
-                  Option two can be something else and selecting it will deselect option one
-                </Label>
-              </FormGroup>
-              <FormGroup check disabled>
-                <Label check>
-                  <Input type="radio" name="radio1" disabled />{' '}
-                  Option three is disabled
+                  Applying to be a student
                 </Label>
               </FormGroup>
             </FormGroup>
             <FormGroup check>
               <Label check>
                 <Input type="checkbox" />{' '}
-                Check me out
+                I'm currently enrolled in a ballet program
               </Label>
             </FormGroup>
             <Button>Submit</Button>
           </Form>
         </Container>
-			</div>
+			</Jumbotron>
     );
   }
 }
