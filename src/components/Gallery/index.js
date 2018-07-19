3b499344044c867aba04369659dd08577077f52e
David Cuthell <dcc1447@gmail.com>
2018-07-19 14:23:50 -0700

    change layout for gallery to include grid, remove media tags

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,35 +1,71 @@
 import React, { Component } from 'react';
 import './styles.css';
-import { Media } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 
 import YouTubeFrame from '../YouTubeFrame';
 
 class Gallery extends Component {
 render() {
+	console.log(window.innerWidth);
+	console.log(window.innerHeight);
+	let winX = 1200;
+	let winY = 1000;
+	if(window.innerWidth < 1200){
+		if(window.innerWidth < 992){
+			if(window.innerWidth < 768){
+				if(window.innerWidth < 576){
+					winX = 300;
+				}
+				else{
+					winX = 550;
+				}
+			}
+			else{
+				winX = 750;
+			}
+		}else{
+			winX = 950;
+		}
+	}
+
+	if(window.innerHeight < 1000){
+		if(window.innerHeight < 792){
+			if(window.innerHeight < 568){
+				if(window.innerHeight < 276){
+					winY = 300;
+				}
+				else{
+					winY = 400;
+				}
+			}
+			else{
+				winY = 600;
+			}
+		}else{
+			winY = 800;
+		}
+	}
+	console.log(winX);
   return(
-  	<div>
-      <Media>
-       <Media right href="#">
-        <YouTubeFrame src="PLXeXlqUgjSWOq9BGX4j4WbzBJ5OC7OpUD"/>
-       </Media>
-       <Media body>
-         <Media heading>
-           YouTube Video Playlists
-         </Media>
-         Playlists can be changed through the youtube account
-       </Media>
-     	</Media>
-     	<Media>
-	      <Media right href="#">
-	       <YouTubeFrame src="PLXeXlqUgjSWNzdoC-wIJwJaQ-8jMqth6G"/>
-	      </Media>
-	      <Media body>
-	        <Media heading>
-	          Individual Videos
-	        </Media>
-	        Could also be added
-	      </Media>
-    	</Media>
+  	<div className="Gallery">
+      <Container>
+				<Row>
+					<Col>
+						<YouTubeFrame src="PLXeXlqUgjSWOq9BGX4j4WbzBJ5OC7OpUD"/>
+					</Col>
+					<Col>
+						<p>Words</p>
+					</Col>
+				</Row>
+				<Row>
+					<Col sm="9">
+						<YouTubeFrame src="PLXeXlqUgjSWNzdoC-wIJwJaQ-8jMqth6G"/>
+					</Col>
+					<Col sm="3">
+						<p>More Words</p>
+					</Col>
+				</Row>
+			</Container>
 		</div>
     );
   }
