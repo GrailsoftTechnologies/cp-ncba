62af46c58bfcc89e745ddb20deb9ddbba9332aa2
David Cuthell <dcc1447@gmail.com>
2018-07-19 14:38:13 -0700

    add fake responsiveness to ytframes

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -8,50 +8,54 @@ class Gallery extends Component {
 render() {
 	console.log(window.innerWidth);
 	console.log(window.innerHeight);
-	let winX = 1200;
-	let winY = 1000;
+	let winX = 768;
+	let winY = 468;
 	if(window.innerWidth < 1200){
 		if(window.innerWidth < 992){
 			if(window.innerWidth < 768){
 				if(window.innerWidth < 576){
 					winX = 300;
+					winY = 200;
 				}
 				else{
-					winX = 550;
+					winX = 457;
+					winY = 278;
 				}
 			}
 			else{
-				winX = 750;
+				winX = 640;
+				winY = 390;
 			}
 		}else{
-			winX = 950;
+			winX = 768;
+			winY = 468;
 		}
 	}
 
-	if(window.innerHeight < 1000){
-		if(window.innerHeight < 792){
-			if(window.innerHeight < 568){
-				if(window.innerHeight < 276){
-					winY = 300;
-				}
-				else{
-					winY = 400;
-				}
-			}
-			else{
-				winY = 600;
-			}
-		}else{
-			winY = 800;
-		}
-	}
+	// if(window.innerHeight < 1000){
+	// 	if(window.innerHeight < 792){
+	// 		if(window.innerHeight < 568){
+	// 			if(window.innerHeight < 276){
+	//
+	// 			}
+	// 			else{
+	//
+	// 			}
+	// 		}
+	// 		else{
+	//
+	// 		}
+	// 	}else{
+	//
+	// 	}
+	// }
 	console.log(winX);
   return(
   	<div className="Gallery">
       <Container>
 				<Row>
 					<Col>
-						<YouTubeFrame src="PLXeXlqUgjSWOq9BGX4j4WbzBJ5OC7OpUD"/>
+						<YouTubeFrame src="PLXeXlqUgjSWOq9BGX4j4WbzBJ5OC7OpUD" height={winY} width={winX}/>
 					</Col>
 					<Col>
 						<p>Words</p>
