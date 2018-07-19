9879d1394e3dbd5f9e8b828b2a61ccd637b9746f
David Cuthell <dcc1447@gmail.com>
2018-07-19 08:26:18 -0700

    change ytframes to use https

--- src/components/YouTubeFrame/index.js
+++ src/components/YouTubeFrame/index.js
@@ -6,7 +6,7 @@ class YouTubeFrame extends Component {
     return (
       <div>
         <iframe id="player" type="text/html" width={this.props.width} height={this.props.height}
-  src="http://www.youtube.com/embed?listType=playlist&list=PLXeXlqUgjSWNAZFPJB6rf7uOBUtvEjfJD"
+  src={"https://www.youtube.com/embed?listType=playlist&list=" + this.props.src}
   frameBorder="0" title="YTplaylist"></iframe>
       </div>
     );
