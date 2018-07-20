5a288bc00ef68417bbf68965cae0877fb9101854
David Cuthell <dcc1447@gmail.com>
2018-07-20 16:00:27 -0700

    removed cards from home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -58,41 +58,6 @@ class Home extends Component {
               </Jumbotron>
             </Col>
           </Row>
-          <Row>
-            <Col>
-              <Card>
-                <CardImg top width="100%" src={pic1} alt="Card image cap" />
-                <CardBody>
-                  <CardTitle>Card title</CardTitle>
-                  <CardSubtitle>Card subtitle</CardSubtitle>
-                  <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                  <Button>Button</Button>
-                </CardBody>
-              </Card>
-            </Col>
-            <Col>
-              <Card>
-                <CardImg top width="100%" src={pic2} alt="Card image cap" />
-                <CardBody>
-                  <CardTitle>Card title</CardTitle>
-                  <CardSubtitle>Card subtitle</CardSubtitle>
-                  <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                  <Button>Button</Button>
-                </CardBody>
-              </Card>
-            </Col>
-            <Col>
-              <Card>
-                <CardImg top width="100%" src={pic3} alt="Card image cap" />
-                <CardBody>
-                  <CardTitle>Card title</CardTitle>
-                  <CardSubtitle>Card subtitle</CardSubtitle>
-                  <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                  <Button>Button</Button>
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
         </Container>
 			</div>
 		)
