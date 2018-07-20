a92107a4bc64c7c19dc5275ad1a7afa259eae1e2
David Cuthell <dcc1447@gmail.com>
2018-07-20 15:57:03 -0700

    changed content for about section, styling

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,20 +5,38 @@ import './styles.css';
 class About extends Component {
 render() {
   return(
-      <div>
-        <Container>
-          <Row>
-            <Col>
-              <p>
-                After completing a career as a professional ballet dancer with San Francisco Ballet, Aaron Orza has grown into the role of freelance Ballet Master. He received mentorship from the best the ballet world has to offer and has had the privilege of performing with world-renowned companies and artists. His first big break came when he was just fourteen. Helgi Tommoson, the Director of the San Francisco Ballet, selected him to perform with the company in their rendition of George Balanchine’s Stars and Stripes. From there, he continued his training with the San Francisco Ballet School and the School of American Ballet, in New York City, under full scholarship. Aaron received his next big break at 17 years old when he received his first professional contract with the San Francisco Ballet. During his twelve seasons with the company, he successfully performed numerous Corps de Ballet, Soloist, and Principal roles, and gained acclaim from his colleagues and critics alike. As well as touring the world performing at the most beautiful Opera Houses and stages this world has to offer.
-              </p>
-              <p>
-                Today, Aaron is proud to call Nashville, Tennessee home. His passion for dance has lead him to his new role as mentor for dancers that have dreams of succeeding in the art form he has mastered. If you choose to work with Aaron, your knowledge and skills will improve drastically. As you will find out quickly you will be receiving first hand knowledge that could have only been passed down from generation to generation from leaders and legends of the ballet artform. For a consultation, feel free to contact him at Aaron.Orza@gmail.com
-              </p>
-            </Col>
-          </Row>
-        </Container>
-			</div>
+      <Container className="About">
+        <Row>
+          <Col>
+						<h3>
+							A little about Aaron...
+						</h3>
+            <p>
+              San Francisco native Aaron Orza is from a dancing family.  Both his parents danced with the Oakland Ballet and all five of his siblings have danced professionally with either San Francisco Ballet or Pacific Northwest Ballet.  Aaron began his training at the age of eight at City Ballet of San Francisco under Damara Bennett, later moving to Marin Ballet under the direction of Margaret Swarthout.  At the age of eleven, he received a full scholarship to the San Francisco Ballet School where he trained under Jorge Esquivel, Ricardo Bustamante, Irina Jacobson, Parrish Maynard and Johan Renvall, among others.  Aaron also attended summer programs at the School of American Ballet, where he was accepted for the year-round program on full scholarship.  At age 17, he returned to San Francisco Ballet as an apprentice.  Three months later, he was offered a contract as a full member of the Company.
+            </p>
+						<hr className="my-2" />
+						<p>
+							During his twelve seasons with the San Francisco Ballet, Aaron was privileged to perform Corps de Ballet, Soloist and Principal roles in works by Georges Balanchine, Jerome Robbins, Paul Taylor, Val Caniparoli and many others.  His career brought praise from colleagues and critics alike and gave him the opportunity to perform at the highest standard in some of the most beautiful opera houses and stages this world has to offer.
+						</p>
+						<hr className="my-2" />
+						<p>
+							After retiring from San Francisco Ballet, Aaron discovered a passion for teaching and coaching classical ballet.  As a teacher and coach, Aaron both respects and enjoys the opportunity to pass on to the next generation the technical expertise and artistic mentorship he received throughout his career.  Aaron has held faculty and guest teacher positions at schools and professional companies throughout the United States, including Contra Costa Ballet School, Company C Contemporary Ballet, _____[Aaron - fill in the blanks].  From 2015-17, he was a teacher and Ballet Master at the Rock School for Dance Education where he also privately coached students for the Youth America Grand Prix.  Several of his students advanced to the final rounds in New York, where they were awarded scholarships to attend pre-professional schools and programs both in the U.S. and abroad.
+						</p>
+						<hr className="my-2" />
+						<p>
+							Today, Aaron is proud to call Nashville, Tennessee home and is delighted to have the opportunity to bring his passion for the art of dance to Nashville Classical Academy.
+						</p>
+						<hr className="my-2" />
+						<h3>
+							About Nashville Classical Ballet Academy
+						</h3>
+						<p>
+							Nashville Classical Ballet Academy offers pre-professional classical ballet training and individual coaching for students at Levels ____.  Our goal is to create a supportive, positive environment for learning that fosters personal growth in all our students and brings out the best in dancers from all backgrounds.
+
+						</p>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
