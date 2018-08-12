6247a657a691671fe6b185884ca7389ed15071f9
David Cuthell <dcc1447@gmail.com>
2018-08-12 17:35:33 -0600

    make customer changes to programs section

--- src/components/Programs/index.js
+++ src/components/Programs/index.js
@@ -3,8 +3,8 @@ import { Container, Row, Col, Jumbotron, Button,
   Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
 import './styles.css';
 
-import pic1 from '../../img/1.jpg';
-import pic2 from '../../img/2.jpg';
+import pic1 from '../../img/image2.jpeg';
+import pic2 from '../../img/image3.jpeg';
 import pic3 from '../../img/3.jpg';
 
 class Programs extends Component {
@@ -17,9 +17,6 @@ render() {
 							<Jumbotron fluid>
 				        <Container fluid>
 				          <h1 className="display-3">Program Information</h1>
-				          <p className="lead">
-										Text to come
-									</p>
 				        </Container>
 				      </Jumbotron>
 						</Col>
@@ -29,9 +26,11 @@ render() {
               <Card>
                 <CardImg top width="100%" src={pic1} alt="Card image cap" />
                 <CardBody>
-                  <CardTitle>How to Enroll</CardTitle>
+                  <CardTitle>Audition and Placement</CardTitle>
                   <CardSubtitle></CardSubtitle>
-                  <CardText>Please email Assistant Director Kristin Lindsay for inquiries. A private lesson will be required for all new students interested in enrolling the Nashville Classical Ballet Academy. Parents are strongly encouraged to observe. Families will be contacted shortly after the private lesson for further discussion, and from there may be invited to attend a regular scheduled class. Payment can be made through PayPal.</CardText>
+                  <CardText>
+										A private lesson will be required for all new students interested in attending NCBA. Parents are strongly encouraged to observe. Families will be contacted shortly after the private lesson for further discussion and from there may be invited to attend a regular scheduled class. Please email the Assistant Director, Kristin Lindsay for inquiries.
+									</CardText>
                   <Button href={"#/Contact"}>Contact</Button>
                 </CardBody>
               </Card>
@@ -42,8 +41,9 @@ render() {
                 <CardBody>
                   <CardTitle>Curriculum</CardTitle>
                   <CardSubtitle></CardSubtitle>
-                  <CardText>This can be an openining description of what the classes are like, linking to a more detailed description page</CardText>
-                  <Button>Explore</Button>
+                  <CardText>
+										Nashville Classical Ballet Academy is happy to announce we are opening our doors to the great city of Nashville. We are proud to call ourselves the first classical ballet school to offer an educational experience passed down through generations of legendary dancers and master teachers. This elite training  program is dedicated to teaching the highest standard of ballet education to prepare career bound students. In our program we provide technique, pointe, conditioning, and private coaching to excel the abilities of our dancers. We offer a day program to suit the needs of our most advanced level and an evening program fitting to our intermediate level.
+									</CardText>
                 </CardBody>
               </Card>
             </Col>
@@ -51,9 +51,11 @@ render() {
               <Card>
                 <CardImg top width="100%" src={pic3} alt="Card image cap" />
                 <CardBody>
-                  <CardTitle>Pricing and Payment</CardTitle>
-                  <CardText>This text can be a brief lead into the cost of enrollment, as well as how to go about payment through PayPal</CardText>
-                  <Button>Continue</Button>
+                  <CardTitle>Pricing</CardTitle>
+                  <CardText>
+										All pricing and tuition inquires please email info@ncbacademy.com
+									</CardText>
+                  <Button href={"mailto:info@ncbacademy.com"}>Contact</Button>
                 </CardBody>
               </Card>
             </Col>
