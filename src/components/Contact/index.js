a88368c519f1531ce8131a3cd80e0f43d175c3a9
David Cuthell <dcc1447@gmail.com>
2018-08-12 17:56:31 -0600

    made customer changes to contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -12,12 +12,26 @@ class Contact extends Component {
 							<h1 className="display-3">Aaron Orza</h1>
 							<h1 className="display-4">Artistic Director</h1>
 							<a href="tel:+14156134104">(415)-613-4104</a>
+							<p><a href="mailto:aaron@ncbacademy.com">aaron@ncbacademy.com</a></p>
 							<hr className="my-2"></hr>
 							<h1 className="display-3">Kristin Lindsay</h1>
 							<h1 className="display-4">Assistant Director</h1>
 							<a href="tel:+14152720481">(415)-272-0481</a>
+							<p><a href="mailto:kristin@ncbacademy.com">kristin@ncbacademy.com</a></p>
 							<hr className="my-2"></hr>
 							<a href="mailto:info@ncbacademy.com">info@ncbacademy.com</a>
+						</Jumbotron>
+					</Col>
+				</Row>
+				<Row>
+					<Col>
+						<Jumbotron fluid>
+							<p>
+								During our search for a new building, the Milele Academy has graciously allowed us to rent studio space. We will be holding all classes and private lessons here at this time. Please direct all questions, emails and phone calls to info@ncbacademy.com
+							</p>
+							<p>The Milele Academy</p>
+							<p>1919 Church Street</p>
+							<p>Nashville, TN 37203</p>
 						</Jumbotron>
 					</Col>
 				</Row>
