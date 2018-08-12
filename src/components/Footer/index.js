02fedbe89d4e6427f887728a12505a207729e03a
David Cuthell <dcc1447@gmail.com>
2018-08-12 16:58:16 -0600

    make customer changes to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -15,14 +15,12 @@ render() {
 				</Col>
 				<Col sm="6" className="addressBox">
 					<p><strong>Nashville Classical Ballet Academy</strong></p>
-					<p>123 Main St.</p>
-					<p>Nashville TN 12345</p>
-					<p>503-867-5309</p>
+					<p>info@ncbacademy.com</p>
+					<p>(415)-613-4104</p>
 				</Col>
 				<Col sm="3">
 					<a href="http://www.facebook.com"><i className="mediaIcon fab fa-facebook"></i></a>
 					<a href="http://www.instagram.com"><i className="mediaIcon fab fa-instagram"></i></a>
-					<a href="http://www.twitter.com"><i className="mediaIcon fab fa-twitter"></i></a>
 					<a href="http://www.youtube.com"><i className="mediaIcon fab fa-youtube"></i></a>
 				</Col>
 			</Row>
