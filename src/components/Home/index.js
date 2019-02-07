3f2b89c60b5eb70b39272a9c696194c51dcab06d
David Cuthell <dcc1447@gmail.com>
2019-02-07 10:52:13 -0800

    update sitemaps, remove unused imgs, clean up test code

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,7 +3,6 @@ import './styles.css';
 import { Container, Row, Col, Jumbotron } from 'reactstrap';
 
 import CarouselFrame from '../CarouselFrame';
-import InstagramFrame from '../InstagramFrame';
 
 class Home extends Component {
   render() {
@@ -17,7 +16,6 @@ class Home extends Component {
 	              <h1 id="missionText">
 									Nashville Classical Ballet Academy is designed to meet the needs of the dedicated student seeking a professional career in ballet. We offer an elite ballet education, providing students with a refined and sophisticated level of training and self-discipline to thrive in any professional environment. We strive to share our passion for classical ballet, our training and experience in an environment with which all our students can reach success.
 								</h1>
-                <InstagramFrame />
 	            </Jumbotron>
 	          </Col>
 	        </Row>
