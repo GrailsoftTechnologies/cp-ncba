3279a70f98d5b7e3bfd6c1ac0209ddea62fbdf11
David Cuthell <dcc1447@gmail.com>
2019-03-04 23:59:47 -0800

    add ig code

---
+++ src/components/IGEmbedFrame/index.js
@@ -0,0 +1,124 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Container, Col, Row } from 'reactstrap'
+
+import IGFrame from '../IGFrame'
+import IGSquare from '../IGSquare'
+
+// Add real shit heres the code  e98b264cbe634b879b165f7f3f46bd15
+class IGEmbedFrame extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      urls: [],
+      imgs: [],
+      captions: [],
+      height: (parseInt(window.innerHeight * 0.80, 10) + 61), // 10 is for radix
+      width: ((window.innerWidth > 576) ? 500 : parseInt(window.innerWidth * 0.75, 10))
+    }
+  }
+
+  mapSquaresToState() {
+    fetch('https://www.thunderbirdbarpdx.com/sup.php')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          let results = JSON.parse(result)
+          let myMap = results.data
+          myMap.forEach((data) => {
+            let newurl = data.link.replace('/thunderbirdpdx', '')
+            this.setState({
+              urls: [
+                ...this.state.urls,
+                newurl
+              ],
+              imgs: [
+                ...this.state.imgs,
+                data.images.low_resolution.url
+              ],
+              captions: [
+                ...this.state.captions,
+                data.caption.text
+              ]
+            })
+          })
+        },
+        (error) => {
+          if (error) {
+            console.log(error)
+          }
+          this.setState({
+            urls: ['error']
+          })
+        }
+      )
+  }
+
+  componentWillMount() {
+    if (this.props.source === '') {
+      this.mapSquaresToState()
+    } else {
+      this.setState({
+        urls: [this.props.source]
+      })
+    }
+  }
+
+  render() {
+    let list = null
+    if (this.state.urls[0] !== 'error' && !this.props.squares) {
+      list = this.state.urls.map((url, index) =>
+        <IGFrame
+          source={this.state.urls[index]}
+          key={index}
+        />
+      )
+      return (
+        <div className='IGEmbedFrame' style={{textAlign: 'center', height: this.state.height, width: this.state.width}}>
+          <h4 style={{height: '41px', marginTop: '20px'}}>Instagram Posts</h4>
+          <div style={{textAlign: 'center', overflow: 'scroll', height: (this.state.height - 61)}}>
+            {list}
+          </div>
+        </div>
+      )
+    } else if (this.state.urls[0] !== 'error' && this.props.squares) {
+      list = this.state.urls.map((url, index) =>
+        <Col key={index} style={{padding: '0px'}}>
+          <IGSquare
+            url={this.state.urls[index]}
+            img={this.state.imgs[index]}
+            caption={this.state.captions[index]}
+          />
+        </Col>
+      )
+      return (
+        <div className='IGEmbedFrame' style={{textAlign: 'center', backgroundColor: 'black'}}>
+          <h4 style={{height: '41px', marginTop: '20px'}}>Instagram Posts</h4>
+          <Container>
+            <Row style={{margin: '0px'}}>
+              {list}
+            </Row>
+          </Container>
+        </div>
+      )
+    } else {
+      return (
+        <p>There was an error fetching the Instagram Feed</p>
+      )
+    }
+  }
+}
+
+IGEmbedFrame.defaultProps = {
+  source: '', // Should be in the form of https://www.instagram.com/p/BpnXsmIFYXu/
+  // token: '',
+  squares: false
+}
+
+IGEmbedFrame.propTypes = {
+  source: PropTypes.string,
+  // token: PropTypes.string,
+  squares: PropTypes.bool
+}
+
+export default IGEmbedFrame
---
+++ src/components/IGSquare/index.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class IGSquare extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      width: ((window.innerWidth > 576) ? 320 : parseInt(window.innerWidth * 0.75, 10)),
+      height: (parseInt(window.innerHeight * 1.1, 10)), // 10 is for radix
+      mobile: !((window.innerWidth > 576) && (window.innerHeight > 576)),
+      active: false
+    }
+    this.handleMouseEnter = this.handleMouseEnter.bind(this)
+    this.handleMouseLeave = this.handleMouseLeave.bind(this)
+    this.handleTouchStart = this.handleTouchStart.bind(this)
+  }
+
+  handleMouseEnter(e) {
+    this.setState({
+      active: true
+    })
+  }
+
+  handleMouseLeave(e) {
+    this.setState({
+      active: false
+    })
+  }
+
+  handleTouchStart(e) {
+    this.setState({
+      active: (!this.state.active)
+    })
+  }
+
+  render() {
+    return (
+      <div style={{textAlign: 'center', margin: 'auto', width: this.state.width, height: '320px'}}
+        onMouseEnter={this.handleMouseEnter}
+        onMouseLeave={this.handleMouseLeave}
+        onTouchStart={this.handleTouchStart} >
+        <a href={this.props.url}>
+          <div className='d-flex flex-column align-items-center' style={{position: 'absolute', backgroundColor: 'black', opacity: 0.8, zIndex: 3, width: this.state.width, height: '320px', visibility: this.state.active ? 'visible' : 'hidden'}}>
+            <p style={{margin: 'auto'}}>{this.props.caption}</p>
+          </div>
+        </a>
+        <a href={this.props.url} style={{zIndex: 2}} >
+          <img
+            src={this.props.img}
+            alt={this.props.caption}
+            style={{width: this.state.width, height: '320px'}}
+          />
+        </a>
+      </div>
+    )
+  }
+}
+
+IGSquare.defaultProps = {
+  url: 'https://www.instagram.com/p/Bl4m0xInwQl/',
+  img: '',
+  caption: ''
+}
+
+IGSquare.propTypes = {
+  url: PropTypes.string,
+  img: PropTypes.string,
+  caption: PropTypes.string
+}
+
+export default IGSquare
