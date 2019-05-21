5be7ada749a5730e743f7cddfce053c169b8f9b3
David Cuthell <dcc1447@gmail.com>
2019-05-21 12:58:44 -0700

    add donation button to homepage

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,8 +3,31 @@ import './styles.css';
 import { Container, Row, Col, Jumbotron } from 'reactstrap';
 
 import CarouselFrame from '../CarouselFrame';
+import logo from '../../img/logo.png'
 
 class Home extends Component {
+
+  componentWillMount(){
+    window.paypal.Buttons({
+        createOrder: function(data, actions) {
+            return actions.order.create({
+                purchase_units: [{
+                    amount: {
+                        value: '0.68'
+                    },
+                    description: 'Your generous contribution'
+                }]
+            });
+        },
+        onApprove: function(data, actions) {
+            return actions.order.capture().then(function(details) {
+
+                alert('Transaction completed by ' + details.payer.name.given_name + '!');
+            });
+        }
+    }).render('#paypal-button-container');
+  }
+
   render() {
 		return(
 			<div className="Home">
@@ -16,16 +39,18 @@ class Home extends Component {
 	              <h1 id="missionText">
 									Nashville Classical Ballet Academy is designed to meet the needs of the dedicated student seeking a professional career in ballet. We offer an elite ballet education, providing students with a refined and sophisticated level of training and self-discipline to thrive in any professional environment. We strive to share our passion for classical ballet, our training and experience in an environment with which all our students can reach success.
 								</h1>
+                <hr />
+                <br />
+                <p>You can support us by clicking on the donate button below!</p>
+                  <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                    <input type="hidden" name="cmd" value="_s-xclick" />
+                    <input type="hidden" name="hosted_button_id" value="6WS9J2KXMAVAN" />
+                    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+                    <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+                  </form>
 	            </Jumbotron>
 	          </Col>
 	        </Row>
-          <Row>
-            <Col>
-              <Jumbotron fluid>
-                <div id="paypal-button-container" />
-              </Jumbotron>
-            </Col>
-          </Row>
 	      </Container>
 			</div>
 		)
