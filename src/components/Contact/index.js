4208e3807a36781ea82f6be9b93dc82136d84a65
David Cuthell <dcc1447@gmail.com>
2019-05-21 13:02:52 -0700

    add donation button to contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -58,6 +58,15 @@ class Contact extends Component {
 							<p>
 								During our search for a new building, we will be renting studio space within the Nashville Area. For more information, please direct all questions, emails and phone calls to info@ncbacademy.com
 							</p>
+              <hr />
+              <br />
+              <p>To make a donation in support of NCBA please click the link DONATE</p>
+              <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                <input type="hidden" name="cmd" value="_s-xclick" />
+                <input type="hidden" name="hosted_button_id" value="6WS9J2KXMAVAN" />
+                <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+                <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+              </form>
 						</Jumbotron>
 					</Col>
 				</Row>
